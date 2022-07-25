ea621b8a738006a247dafc29dec9f7536e1f122f
punkerrr <say.punker@gmail.com>
2022-07-25 21:53:54 +0300

    added .gitignore added allure added test dispay name

--- src/test/java/CatalogTest.java
+++ src/test/java/CatalogTest.java
@@ -1,4 +1,6 @@
+import io.qameta.allure.Step;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pages.CatalogPage;
 
@@ -8,6 +10,8 @@ public class CatalogTest extends BaseTest {
     private final static String DICOUNT="-15%";
 
     @Test
+    @Step("Проверка дискаунта")
+    @DisplayName("Проверка дискаунта")
     public void testCheckDiscount(){
         CatalogPage catalogPage = new CatalogPage();
         catalogPage.openCatalog(BASE_URL);
