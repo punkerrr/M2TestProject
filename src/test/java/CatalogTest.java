80bd2b13fd40ec78e4e8596b76e77439b9acf42e
punkerrr <say.punker@gmail.com>
2022-07-26 02:49:04 +0300

    fixed logic

--- src/test/java/CatalogTest.java
+++ src/test/java/CatalogTest.java
@@ -7,15 +7,19 @@ import pages.CatalogPage;
 public class CatalogTest extends BaseTest {
 
     private final static String BASE_URL="https://v-lot.ru/catalog";
-    private final static String DICOUNT="-15%";
+    private final static String DICOUNT="-29%";
 
+    /** Допустим что в Адыгея есть стабильный лот с названием "2-комн. квартира, 36,5 м², 4/5 эт." у которого есть стабильное значение дисканута.
+     * Надеюсь, что оно не поменяется, потому что без сеттеинга данных нужных данных их соотвествие сложно гарантировать
+    */
     @Test
     @Step("Проверка дискаунта")
     @DisplayName("Проверка дискаунта")
     public void testCheckDiscount(){
         CatalogPage catalogPage = new CatalogPage();
         catalogPage.openCatalog(BASE_URL);
-        catalogPage.setDiscountCheckBoxActive();
+        catalogPage.setRegion("Адыгея");
+        catalogPage.findArticulesWithDiscount();
         Assertions.assertEquals(catalogPage.getDiscountsFromCatalog(),DICOUNT);
 
 
--- src/test/java/pages/CatalogPage.java
+++ src/test/java/pages/CatalogPage.java
@@ -2,12 +2,17 @@ package pages;
 
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.Keys;
 
 import static com.codeborne.selenide.Selenide.$x;
 
 public class CatalogPage {
-    private final SelenideElement discountElement = $x("//div[contains(@class, 'block_discountText')]");
+    private final SelenideElement discountElement = $x("//span[contains(text(),'2-комн. квартира, 36,5 м², 4/5 эт.')]//ancestor::div[1]/following-sibling::div//*[contains(@class, 'block_discountText')]");
     private final SelenideElement checkBoxDiscount = $x("//div[text()='Только с дисконтом']/parent::*");
+    private final SelenideElement buttonFind = $x("//span[text()='Найти']/parent::*");
+    private final SelenideElement regionsDropdown = $x("//div[text()='Регион']/parent::*");
+    private final SelenideElement regionsInput = $x("//input[contains(@class, 'input_no-autofill')]");
+    private final SelenideElement regionsResultFirstRow = $x("//div[contains(@class,'multiselect')]");
 
     public void openCatalog ( String url){
         Selenide.open(url);
@@ -17,8 +22,16 @@ public class CatalogPage {
         return discountElement.getText();
     }
 
-    public void setDiscountCheckBoxActive(){
+    public void findArticulesWithDiscount(){
         checkBoxDiscount.click();
+        buttonFind.click();
+    }
+
+    public void setRegion( String region){
+        regionsDropdown.click();
+        regionsInput.sendKeys(region);
+        regionsResultFirstRow.click();
+        regionsInput.sendKeys(Keys.ESCAPE);
 
     }
 
